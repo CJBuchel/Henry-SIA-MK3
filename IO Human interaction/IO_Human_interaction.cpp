ecb74ed1aa8b51a2382d2182560dcaa4835badf3
CJBuchel <connorbuchel@gmail.com>
2018-02-28 20:10:05 +0800

    Completed Human IO interaction, moving on to SIA

--- IO Human interaction/IO_Human_interaction.cpp
+++ IO Human interaction/IO_Human_interaction.cpp
@@ -16,6 +16,8 @@ char op;
 
 int Initialize_in;
 
+int ERROR_in;
+
 int main(int argc, char const *argv[]) {
 
   cout << "Henry-SIA-MK3, Initialize?" << endl;
@@ -30,30 +32,30 @@ if (Initialize_in == 1) {
   cout << endl << endl << endl << endl;
   system("color 0a");
   cout << "Initializing";
-  Sleep(1000);
+  Sleep(500);
   cout << "-";
-  Sleep(1000);
+  Sleep(500);
   cout << "-";
-  Sleep(1000);
+  Sleep(500);
   cout << "-";
-  Sleep(1000);
+  Sleep(500);
   cout << "-";
-  Sleep(1000);
+  Sleep(500);
   cout << "-";
-  Sleep(1000);
+  Sleep(500);
   cout << "-";
-  Sleep(1000);
+  Sleep(500);
   cout << "-";
-  Sleep(1000);
+  Sleep(500);
   cout << "-";
-  Sleep(1000);
+  Sleep(500);
   cout << "-";
-  Sleep(1000);
+  Sleep(500);
   cout << "-";
   cout << endl;
   cout << "Initialization Complete";
   cout << endl;
-  Sleep(3000);
+  Sleep(1000);
   system("color c7");
   Sleep(300);
   system("color 07");
@@ -70,19 +72,30 @@ if (Initialize_in == 1) {
 
   cout << "WARNING, HENRY/ROBOTIC ARM NOT DETECTED";
   cout << endl;
-  Sleep(2000);
-  cout << "end in";
+  cout << "continue Initialization of SIA? YES OR NO 1/2: ";
+  cin >> ERROR_in;
   cout << endl;
-  Sleep(1000);
-  cout << "3";
-  cout << endl;
-  Sleep(1000);
-  cout << "2";
-  cout << endl;
-  Sleep(1000);
-  cout << "1";
-  cout << endl;
-  Sleep(1000);
+
+  if (ERROR_in == 1) {
+    system ("start C:\\Users\\Conno\\Desktop\\code\\Henry-SIA-MK3\\Henry-SIA-MK3\\SIA\\SIA,Prog\\SIA.exe");
+  }
+
+  if (ERROR_in == 2) {
+    cout << "System Initialize Canceled.";
+    cout << endl;
+    cout << "End In";
+    cout << endl;
+    Sleep(1000);
+    cout << "3";
+    cout << endl;
+    Sleep(1000);
+    cout << "2";
+    cout << endl;
+    Sleep(1000);
+    cout << "1";
+    Sleep(1000);
+  }
+
 
 
 }
---
+++ SIA/SIA,Prog/SIA.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <windows.h>
+
+using namespace std;
+
+
+
+
+
+
+
+int main(int argc, char const *argv[]) {
+
+
+
+
+cout << "waz up";
+Sleep(10000);
+
+
+
+
+
+
+
+
+  return 0;
+}
