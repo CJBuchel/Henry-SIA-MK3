83efb151b730c1656e0563319e94a49857489d35
CJBuchel <connorbuchel@gmail.com>
2018-03-01 22:43:18 +0800

    Over haul, started speech recognition files

--- IO_Human_interaction/IO_Human_interaction.cpp
+++ IO_Human_interaction/IO_Human_interaction.cpp
@@ -20,90 +20,64 @@ int ERROR_in;
 
 int main(int argc, char const *argv[]) {
 
-  cout << "Henry-SIA-MK3, Initialize?" << endl;
-  cout << "YES or NO, type 1 or 2: ";
-  cin >> Initialize_in;
-  cout << endl;
-
-if (Initialize_in == 1) {
   cout << "Initializing SIA Protocals";
   cout << endl;
 
   cout << endl << endl << endl << endl;
   system("color 0a");
   cout << "Initializing";
-  Sleep(500);
+  Sleep(50);
   cout << "-";
-  Sleep(500);
+  Sleep(50);
   cout << "-";
-  Sleep(500);
+  Sleep(50);
   cout << "-";
-  Sleep(500);
+  Sleep(50);
   cout << "-";
-  Sleep(500);
+  Sleep(50);
   cout << "-";
-  Sleep(500);
+  Sleep(50);
   cout << "-";
-  Sleep(500);
+  Sleep(50);
   cout << "-";
-  Sleep(500);
+  Sleep(50);
   cout << "-";
-  Sleep(500);
+  Sleep(50);
   cout << "-";
-  Sleep(500);
+  Sleep(50);
   cout << "-";
   cout << endl;
   cout << "Initialization Complete";
   cout << endl;
   Sleep(1000);
   system("color c7");
-  Sleep(300);
+  Sleep(100);
   system("color 07");
-  Sleep(300);
+  Sleep(100);
   system("color c7");
-  Sleep(300);
+  Sleep(100);
   system("color 07");
-  Sleep(300);
+  Sleep(100);
   system("color c7");
-  Sleep(300);
+  Sleep(100);
   system("color 07");
-  Sleep(300);
+  Sleep(100);
   system("color 0c");
 
   cout << "WARNING, HENRY/ROBOTIC ARM NOT DETECTED";
   cout << endl;
-  cout << "continue Initialization of SIA? YES OR NO 1/2: ";
-  cin >> ERROR_in;
+  cout << "continuing Initialization of SIA ";
+  Sleep(3000);
   cout << endl;
 
-  if (ERROR_in == 1) {
+
     system ("start C:\\Users\\Conno\\Desktop\\code\\Henry-SIA-MK3\\Henry-SIA-MK3\\SIA\\SIA,Prog\\SIA.exe");
-  }
-
-  if (ERROR_in == 2) {
-    cout << "System Initialize Canceled.";
-    cout << endl;
-    cout << "End In";
-    cout << endl;
-    Sleep(1000);
-    cout << "3";
-    cout << endl;
-    Sleep(1000);
-    cout << "2";
-    cout << endl;
-    Sleep(1000);
-    cout << "1";
-    Sleep(1000);
-  }
 
 
 
-}
-if (Initialize_in == 2) {
-  cout << "Initialization Canceled";
-  cout << endl;
-  Sleep(2000);
-}
+
+
+
 
   return 0;
 }
--- SIA/SIA,Prog/SIA.cpp
+++ SIA/SIA,Prog/SIA.cpp
@@ -2,6 +2,12 @@
 #include <string>
 #include <vector>
 #include <windows.h>
+#define MIN_ALL        419
+#define MIN_ALL_UNDO   416
+#include <WinUser.h>
+
+
+
 
 using namespace std;
 
@@ -13,7 +19,18 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
 
+      HWND consoleWindow = GetConsoleWindow(); // This gets the value Windows uses to identify your output window
+      ShowWindow(consoleWindow, SW_MAXIMIZE); // this mimics clicking on its' maximize button
+
+
+
+
+
+
+
+
   system ("color 09");
+
 
 
 
@@ -22,10 +39,23 @@ cout << endl;
 cout << endl;
 cout << endl;
 cout << endl;
+cout << "Hello User";
 cout << endl;
 cout << endl;
+cout << "SIA Activated";
 cout << endl;
-cout << "IO Log" << endl;
+cout << "Output Log:" << endl;
+cout << endl;
+
+system ("start C:\\Users\\Conno\\Desktop\\code\\Henry-SIA-MK3\\Henry-SIA-MK3\\Speach_recognition\\Speech.exe");
+
+
+system ("start C:\\Users\\Conno\\Desktop\\code\\Henry-SIA-MK3\\Henry-SIA-MK3\\Voice\\0001.m4a"); // This gets the value Windows uses to identify your output window
+
+cout << "Voice 0001 'SIA alpha 0.01 Activated'";
+cout << endl;
+
+
 system ("pause");
 
 
